8bec3b78db9212388574baa5503f79f5a07ee44a
borislegenkiy <boris.legenkiy@gmail.com>
2016-07-22 20:10:09 +0300

    layout2
    layout2

---
+++ js/script.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function() {
+	$('.avatar_plus_button').mouseover(function(){
+		if($(this).attr('state')==="image_plus") {
+			$(this).html("<a class='follow_button' href='#'>Follow</a>");
+			$(this).attr("state","follow_button");
+		}
+		if($(this).attr('state')==="image_ok") {
+			$(this).html("<a class='nofollow_button' href='#'>Nofollow</a>");
+			$(this).attr("state","nofollow_button");
+		}
+	});
+	$(".avatar_plus_button").on("mousedown",function() {
+			if ($(this).attr("state")=="follow_button") {
+				$(this).html("<a class='nofollow_button' href='#'>Nofollow</a>");	
+			}
+			if ($(this).attr("state")=="nofollow_button") {
+				$(this).html("<a class='follow_button' href='#'>Follow</a>");	
+				$(this).attr("state","");
+			}
+			if ($(this).attr("state")=="follow_button") {
+				$(this).attr("state","nofollow_button");
+			} else {
+				$(this).attr("state","follow_button");
+			}
+	});
+});
